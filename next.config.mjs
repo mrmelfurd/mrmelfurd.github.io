a0ae7a84ee9b0c7f22988876e34ddcb5f07aefcc
Mel Chi <mel.chi@slalom.com>
2026-05-05 00:28:29 -0400

    initial website

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Uncomment and set if deploying to a subpath:
+  // basePath: '/website',
+  // assetPrefix: '/website/',
+};
+
+export default nextConfig;
